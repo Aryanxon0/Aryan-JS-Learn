4b4463e4268f91a18b7e621542916ad102c30592
Aryanxon0 <aryanpaghda@gmail.com>
2024-12-12 10:53:37 +0530

    Update 01_variabels.js

--- 01_variabels.js
+++ 01_variabels.js
@@ -7,13 +7,15 @@ let accountstate ;
 // accountId = 3 
 
 
-
+/* 
+   !!  Don't use var !!
+*/
 
 accountEmail = "ap@gmail.com"
 accountpassword = "1234"
 accountCity = "jamnagar"
 
 
-console.log(accountId);
+//console.log(accountId);
 
 console.table([accountId ,accountEmail ,accountpassword ,accountCity ,accountstate])
