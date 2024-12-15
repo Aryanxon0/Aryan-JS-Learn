2c388c63a1cbbd692386e2431804bfa31bc23b16
Aryanxon0 <aryanpaghda@gmail.com>
2024-12-15 09:14:08 +0530

    data types- summary

---
+++ datatypes-summary.js
@@ -0,0 +1,36 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 123456778987654567sn
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["spaider man", "tom holend", "salu"];
+let myObj = {
+    name: "aryan",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
